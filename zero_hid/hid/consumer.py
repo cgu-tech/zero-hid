11ff19298a595f05f3a60d7b41d9b7dabd9f938f
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-06 21:13:57 +0200

    Fix Consumer keys

--- zero_hid/hid/consumer.py
+++ zero_hid/hid/consumer.py
@@ -3,8 +3,9 @@
 from . import write as hid_write
 
 def send_keystroke(consumer_path, hid_keycode, release=True) -> None:
-    buf = [0] * 1
-    buf[0] = hid_keycode
+    buf = [0] * 2
+    buf[0] = hid_keycode & 0xFF        # LSB
+    buf[1] = (hid_keycode >> 8) & 0xFF # MSB
     hid_write.write_to_hid_interface(consumer_path, buf)
 
     # If it's a normal keycode (i.e. not a standalone modifier key), add a
@@ -13,5 +14,5 @@ def send_keystroke(consumer_path, hid_keycode, release=True) -> None:
         release_keys(consumer_path)
 
 def release_keys(consumer_path):
-    buf = [0] * 1
+    buf = [0] * 2
     hid_write.write_to_hid_interface(consumer_path, buf)
