07e9b1654b7fc50f3d229b8b84911eb724728f42
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-09 23:35:20 +0200

    Fix bad __init__.py declaration

--- zero_hid/__init__.py
+++ zero_hid/__init__.py
@@ -4,7 +4,7 @@ from .mouse import Mouse
 from .keyboard import Keyboard
 from .hid.keycodes import KeyCodes
 from .consumer import Consumer
-from .hid.consumers import ConsumerCodes
+from .hid.consumercodes import ConsumerCodes
 from . import defaults
 
-__all__ = ["Mouse", "Keyboard", "KeyCodes", "defaults"]
+__all__ = ["Mouse", "Keyboard", "KeyCodes", "Consumer", "ConsumerCodes", "defaults"]
