4a0129d99000c9d3a7b2f560c4175dc1154aba1c
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-16 23:18:00 +0200

    Add report padding

--- zero_hid/hid/consumer.py
+++ zero_hid/hid/consumer.py
@@ -5,13 +5,12 @@ from . import write as hid_write
 CONSUMER_REPORT_ID = 0x02  # Report ID for Consumer Control
 
 def send_keystroke(consumer_path, hid_keycode, release=True) -> None:
-    buf = [0] * 3
+    buf = [0] * 8
     buf[0] = CONSUMER_REPORT_ID        # Report ID
-    buf[1] = (hid_keycode >> 8) & 0xFF # LSB
-    buf[2] = hid_keycode & 0xFF        # MSB
-    
-    #buf[1] = hid_keycode & 0xFF        # LSB
-    #buf[2] = (hid_keycode >> 8) & 0xFF # MSB
+    #buf[1] = (hid_keycode >> 8) & 0xFF # LSB
+    #buf[2] = hid_keycode & 0xFF        # MSB
+    buf[1] = hid_keycode & 0xFF        # LSB
+    buf[2] = (hid_keycode >> 8) & 0xFF # MSB
     hid_write.write_to_hid_interface(consumer_path, buf)
 
     # If it's a normal keycode (i.e. not a standalone modifier key), add a
