9f10613a9438f984a7c53dc65743a6af6e842368
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-18 01:20:38 +0200

    Fix replacing string with file-like object in test

--- tests/mouse_test.py
+++ tests/mouse_test.py
@@ -5,12 +5,14 @@ from common import read_bytes, temp_path
 
 def test_mouse_button_left():
     with temp_path() as p:
-        buttons = [MOUSE_BUTTON_LEFT]
-        x = 0
-        y = 0
-        vertical_wheel_delta = 0
-        horizontal_wheel_delta = 0
-        relative_mouse_event(p, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
-        data = read_bytes(p)
+        with open(path, "r+b") as f:
+            buttons = [MOUSE_BUTTON_LEFT]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(p, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            f.seek(0)
+            data = f.read()
     assert b"\x02\x00\x01\x00\x00\x00\x00\x00" == data
 
