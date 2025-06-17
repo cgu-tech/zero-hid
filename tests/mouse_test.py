9f85110b98fb3ed4e65d5f569733a640ffb40545
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-18 01:35:06 +0200

    Add buttons tests

--- tests/mouse_test.py
+++ tests/mouse_test.py
@@ -16,3 +16,106 @@ def test_mouse_button_left():
             data = dev.read()
     assert b"\x02\x01\x00\x00\x00\x00\x00\x00" == data
 
+def test_mouse_button_right():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_RIGHT]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x02\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_middle():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_MIDDLE]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x04\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_left_and_right():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_LEFT, MOUSE_BUTTON_RIGHT]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x03\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_right_and_left():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_RIGHT, MOUSE_BUTTON_LEFT]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x03\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_left_and_middle():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_LEFT, MOUSE_BUTTON_MIDDLE]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x05\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_midle_and_left():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_MIDDLE, MOUSE_BUTTON_LEFT]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x05\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_right_and_midle():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_RIGHT, MOUSE_BUTTON_MIDDLE]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x06\x00\x00\x00\x00\x00\x00" == data
+
+def test_mouse_button_midle_and_right():
+    with temp_path() as dev_path:
+        with open(dev_path, "w+b") as dev:
+            buttons = [MOUSE_BUTTON_MIDDLE, MOUSE_BUTTON_RIGHT]
+            x = 0
+            y = 0
+            vertical_wheel_delta = 0
+            horizontal_wheel_delta = 0
+            relative_mouse_event(dev, buttons, x, y, vertical_wheel_delta, horizontal_wheel_delta)
+            dev.seek(0)
+            data = dev.read()
+    assert b"\x02\x06\x00\x00\x00\x00\x00\x00" == data
