7c22f9d9795d869eac93472558307294b405e9f0
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-21 10:06:26 +0200

    Fix imports

--- zero_hid/__init__.py
+++ zero_hid/__init__.py
@@ -1,9 +1,9 @@
+from . import defaults
 from .device import Device
 from .mouse import Mouse
 from .keyboard import Keyboard
 from .hid.keycodes import KeyCodes
 from .consumer import Consumer
 from .hid.consumercodes import ConsumerCodes
-from . import defaults
 
-__all__ = ["Device", "Mouse", "Keyboard", "KeyCodes", "Consumer", "ConsumerCodes", "defaults"]
+__all__ = ["defaults", "Device", "Mouse", "Keyboard", "KeyCodes", "Consumer", "ConsumerCodes"]
